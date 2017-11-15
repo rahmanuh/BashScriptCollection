b42b899eaa72510e359ec8efc65b222eba8e6eba
rahmanuh <rahmanu.hermawan.4919@student.uu.se>
2017-11-15 19:11:12 +0100

    update

--- countDown.sh
+++ countDown.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # This script will make a countdown and return a alarm sound.
 
-echo "Type time (in second) you want to countdown, followed by [ENTER]:"
+echo "Type time (in integer minute) you want to countdown, followed by [ENTER]:"
 
 read count
 printf "\033c"
 
-for ((i = count; i > 0; i--))
+for ((i = count*60; i > 0; i--))
 	do
 		echo "$i sec left"
 		sleep 1
 	done
-
-play ../../Music/alarm.wav
+echo "cek"
+#play ../../Music/alarm.wav
