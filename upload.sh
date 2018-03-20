24207491187db7200aa455184bbcb9ffbce061aa
rahmanuh <rahmanu.hermawan.4919@student.uu.se>
2018-03-20 16:12:49 +0100

    update

--- upload.sh
+++ upload.sh
@@ -2,10 +2,10 @@
 # This script is for uploading the program to microcontroller ATMega328p on the board for the project
 
 # Type where is the *.elf or *.hex location
-# example: /home/rahmanuh/Documents/01_MSc_EMBEDDED_SYSTEMS/14_THESIS/Dancing_With_Theremin/build/Uno_ACM0/DWT_Project.elf
+# example: /Dancing_With_Theremin/build/Uno_ACM0/DWT_Project.elf
+# usage: ./upload.sh /location
 
 location=$1
-printf "\033c"
 
 /home/rahmanuh/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/bin/avrdude -C/home/rahmanuh/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9/etc/avrdude.conf -v -patmega328p -cusbasp -Pusb -Uflash:w:$location
 
